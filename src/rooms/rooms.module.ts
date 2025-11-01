3657a231cd4d6d231570a73cfc2320e950eca185
anerthy <112020687+anerthy@users.noreply.github.com>
2025-11-01 00:38:31 -0600

    feat: import ConfigModule in RoomsModule for configuration management

--- src/rooms/rooms.module.ts
+++ src/rooms/rooms.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RoomsService } from './rooms.service';
 import { RoomsController } from './rooms.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [RoomsController],
   providers: [RoomsService],
 })
