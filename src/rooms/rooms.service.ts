618b9d351064594b4cfe6b5398afb851d9ec1af2
anerthy <112020687+anerthy@users.noreply.github.com>
2025-11-01 00:13:55 -0600

    feat: enhance RoomsService to generate words and synonyms based on game parameters

--- src/rooms/rooms.service.ts
+++ src/rooms/rooms.service.ts
@@ -3,6 +3,35 @@ import { GoogleGenAI } from '@google/genai';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { UpdateRoomDto } from './dto/update-room.dto';
 import { BasicMessage } from './interfaces/BasicMessage';
+import { CreateGameDto } from 'src/games/dto/create-game.dto';
+
+const SYSTEM_INSTRUCTION = (
+  theme: string,
+  language: string,
+  vocabulary: string,
+  synonymsCount: number,
+) => `
+# Role:
+Eres un generador de palabras para un juego de adivinanzas. Tu tarea es crear una palabra en ${language} junto con sinónimos que ayuden a los jugadores a adivinar la palabra.
+
+# Reglas:
+- La palabra debe estar relacionada con el tema ${theme}.
+- La palabra deber ser utilizada en el vocabulario de ${vocabulary}.
+- Debes generar exactamente ${synonymsCount} sinónimos.
+- Los sinonimos deben ser unicamente  una palabra.
+- Los sinónimos no deben ser demasiado obvios, pero deben estar relacionados con la palabra original.
+- No debes incluir la palabra original en los sinónimos.
+- No debes proporcionar definiciones o explicaciones, solo la palabra y los sinónimos.
+- Tu respuesta debe ser unicamente un texto con la palabra y sus sinónimos, sin explicaciones adicionales.
+- Solo texto, nada de markdown.
+
+# Formato de salida:
+Dame una palabra en ${language} relacionada con el tema ${theme} y el vocabulario ${vocabulary} y dame ${synonymsCount} palabras que sean sinónimos que ayuden a adivinar la palabra original, pero no menciones directamente la palabra en las pistas.
+
+El resultado debe tener el siguiente formato JSON:
+{"word":"<palabra generada>","synonyms":["<sinónimo 1>","<sinónimo 2>"]}
+# Ejemplo de salida esperada:
+{"word":"Autobús","synonyms":["Transporte","Vehículo"]}`;
 
 @Injectable()
 export class RoomsService {
@@ -12,44 +41,54 @@ export class RoomsService {
 
   private chatHistory = new Map<string, BasicMessage[]>();
 
-  async start(gameId: string) {
-    const gameOptions = {
-      theme: 'Objeto de cocina',
-      language: 'es',
-      vocabulary: 'Costa Rica',
-      minLength: 6,
-      maxLength: 10,
-      synonymsCount: 2,
-    };
+  async start(roomId: string, createGameDto: CreateGameDto) {
+    const { theme, language, vocabulary, outsiders } = createGameDto;
 
-    const response = await this.ai.models.generateContent({
+    const history = this.chatHistory.get(roomId) || [];
+
+    const instructionMessage = SYSTEM_INSTRUCTION(
+      theme,
+      language,
+      vocabulary,
+      outsiders,
+    );
+
+    const chat = this.ai.chats.create({
       model: 'gemini-2.5-flash',
-      contents: `Quiero que generes una palabra en español relacionada con el siguiente tema: ${gameOptions.theme}.
-
-        La palabra debe ser concreta y común en ese campo.
-        La palabra debe tener entre ${gameOptions.minLength} y ${gameOptions.maxLength} letras y no debe ser demasiado obvia.
-        La palabra deber ser utilizada en el vocabulario de ${gameOptions.vocabulary}.
-
-        Luego, dame ${gameOptions.synonymsCount} palabras que sean sinónimos que ayuden a adivinar la palabra original, pero no menciones directamente la palabra en las pistas.
-
-        El resultado debe tener el siguiente formato JSON:
-        {
-        "word": "<palabra generada>",
-        "synonyms": ["<sinónimo 1>", "<sinónimo 2>", ...]
-        }
-
-        Ejemplo de salida esperada:
-        {
-        "word":"Autobús",
-        "synonyms": ["Transporte", "Vehículo"]
-        }
-
-        Tu respuesta debe ser unicamente un JSON con tu informacion de las palabras`,
+      history: history.map((message) => ({
+        role: message.role,
+        parts: [{ text: message.content }],
+      })),
+      config: {
+        systemInstruction: instructionMessage,
+        thinkingConfig: {
+          includeThoughts: false,
+          thinkingBudget: -1, // unlimited
+        },
+      },
     });
-    console.log(response);
-    return {
-      response: response.text,
-    };
+
+    const response = await chat.sendMessage({
+      message: 'Genera una palabra y los sinónimos para el juego.',
+    });
+
+    const subjectJson = response.text
+      ? JSON.parse(response.text)
+      : { word: '', synonyms: [] };
+
+    history.push({
+      role: 'user',
+      content: 'Genera una nueva palabra y los sinónimos para el juego.',
+    });
+
+    history.push({
+      role: 'model',
+      content: response.text || '',
+    });
+
+    this.chatHistory.set(roomId, history);
+
+    return subjectJson;
   }
 
   create(createRoomDto: CreateRoomDto) {
