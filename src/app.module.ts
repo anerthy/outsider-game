06aad54318f502d7a0bd25f94c0fc838837ae5d3
anerthy <112020687+anerthy@users.noreply.github.com>
2025-11-01 00:38:21 -0600

    feat: add @nestjs/config dependency for configuration management

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoomsModule } from './rooms/rooms.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [RoomsModule],
+  imports: [
+    RoomsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
